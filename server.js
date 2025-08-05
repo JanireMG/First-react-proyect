60b397b8ccb2f2754ce4a64db8f4c0527e7faa62
Janire <janiremartinez1997@gmail.com>
2025-08-05 19:48:08 +0200

    Fix start script to run server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
-app.listen(port);
+app.use(express.static(path.join(__dirname, 'dist')));
 
-console.log("server started");
+app.get('*',(req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
